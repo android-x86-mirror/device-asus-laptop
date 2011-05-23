0fa97267c6ea4aa0dcd3046a35dd6fdded861025
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-05-23 11:32:55 +0800

    include Radeon firmwares to support AMD video chipsets

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := iwlwifi-1000-3.ucode iwlwifi-5000-2.ucode
+LOCAL_FIRMWARES := iwlwifi-1000-3.ucode iwlwifi-5000-2.ucode $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/*))
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/laptop_info
 TARGET_PREBUILT_APPS := $(subst $(LOCAL_PATH)/,,$(wildcard $(LOCAL_PATH)/app/*))
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/laptop_defconfig
