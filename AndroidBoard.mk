37b7183539ba8468196e59520e61f6ea3bb320ac
Chih-Wei Huang <cwhuang@linux.org.tw>
2011-08-08 22:38:02 +0800

    update for kernel 2.6.39 and new mesa library

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_FIRMWARES := iwlwifi-1000-3.ucode iwlwifi-5000-2.ucode $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/*))
+LOCAL_FIRMWARES := iwlwifi-1000-5.ucode iwlwifi-5000-5.ucode $(subst device/common/firmware/,,$(wildcard device/common/firmware/radeon/*))
 TARGET_INITRD_SCRIPTS := $(LOCAL_PATH)/laptop_info
 TARGET_KERNEL_CONFIG := $(LOCAL_PATH)/laptop_defconfig
 
--- laptop.mk
+++ laptop.mk
@@ -1,7 +1,7 @@
 PRODUCT_PACKAGES := $(THIRD_PARTY_APPS)
 
 PRODUCT_COPY_FILES := \
-    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
 $(call inherit-product,$(SRC_TARGET_DIR)/product/generic_x86.mk)
 
